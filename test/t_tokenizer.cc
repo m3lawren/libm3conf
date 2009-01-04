4968743dc14459453e3ccf327d45281ef47789a9
Matt Lawrence <m3lawren@uwaterloo.ca>
2009-01-04 00:28:21 -0500

    Cleaning things up.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- test/t_tokenizer.cc
+++ test/t_tokenizer.cc
@@ -1,32 +1,20 @@
 #include <m3conf/tokenizer.h>
 #include <test/XTest.h>
 
+#define CHECK_TOKEN(toktype, val) XTS_ASSERT_DIFFERS(NULL, c); XTS_ASSERT_EQUALS(toktype, c->type); XTS_ASSERT_EQUALS(std::string(val), c->value); c = c->next;
+#define CHECK_EOF() CHECK_TOKEN(TOK_EOF, ""); XTS_ASSERT_EQUALS(NULL, c);
+
 XTS_TEST(TokenizerTests, testSingleChars) {
 	struct Token* t;
 	struct Token* c;
 	std::string data = "{}=;";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_LB, c->type);
-	XTS_ASSERT_EQUALS(std::string("{"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_RB, c->type);
-	XTS_ASSERT_EQUALS(std::string("}"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EQ, c->type);
-	XTS_ASSERT_EQUALS(std::string("="), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_SEMI, c->type);
-	XTS_ASSERT_EQUALS(std::string(";"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_TOKEN(TOK_LB, "{");
+	CHECK_TOKEN(TOK_RB, "}");
+	CHECK_TOKEN(TOK_EQ, "=");
+	CHECK_TOKEN(TOK_SEMI, ";");
+	CHECK_EOF();
 
 	free_tokens(t);
 }
@@ -37,22 +25,10 @@ XTS_TEST(TokenizerTests, testValidInt) {
 	std::string data = "01234-44";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_INT, c->type);
-	XTS_ASSERT_EQUALS(std::string("0"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_INT, c->type);
-	XTS_ASSERT_EQUALS(std::string("1234"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_INT, c->type);
-	XTS_ASSERT_EQUALS(std::string("-44"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_TOKEN(TOK_INT, "0");
+	CHECK_TOKEN(TOK_INT, "1234");
+	CHECK_TOKEN(TOK_INT, "-44");
+	CHECK_EOF();
 
 	free_tokens(t);
 }
@@ -63,22 +39,10 @@ XTS_TEST(TokenizerTests, testValidStr) {
 	std::string data = "\"aoeu\"\"\\\"quote\"\"\\\\\\\"\"";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_STR, c->type);
-	XTS_ASSERT_EQUALS(std::string("aoeu"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_STR, c->type);
-	XTS_ASSERT_EQUALS(std::string("\"quote"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_STR, c->type);
-	XTS_ASSERT_EQUALS(std::string("\\\""), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_TOKEN(TOK_STR, "aoeu");
+	CHECK_TOKEN(TOK_STR, "\"quote");
+	CHECK_TOKEN(TOK_STR, "\\\"");
+	CHECK_EOF();
 
 	free_tokens(t);
 }
@@ -89,34 +53,13 @@ XTS_TEST(TokenizerTests, testValidId) {
 	std::string data = "aoeu AOEU a1 section _ _._";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("aoeu"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("AOEU"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("a1"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("section"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("_"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_ID, c->type);
-	XTS_ASSERT_EQUALS(std::string("_._"), c->value);
-	c = c->next;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_TOKEN(TOK_ID, "aoeu");
+	CHECK_TOKEN(TOK_ID, "AOEU");
+	CHECK_TOKEN(TOK_ID, "a1");
+	CHECK_TOKEN(TOK_ID, "section");
+	CHECK_TOKEN(TOK_ID, "_");
+	CHECK_TOKEN(TOK_ID, "_._");
+	CHECK_EOF();
 
 	free_tokens(t);
 }
@@ -127,10 +70,7 @@ XTS_TEST(TokenizerTests, testValidWhiteSpace) {
 	std::string data = " \t\n\r";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_EOF();
 
 	free_tokens(t);
 }
@@ -141,10 +81,7 @@ XTS_TEST(TokenizerTests, testValidComment) {
 	std::string data = "#comment \t comment\r\n#comment";
 	XTS_ASSERT_EQUALS(0, tokenize(data.c_str(), data.length(), &t));
 	c = t;
-	XTS_ASSERT_DIFFERS(NULL, c);
-	XTS_ASSERT_EQUALS(TOK_EOF, c->type);
-	XTS_ASSERT_EQUALS(std::string(""), c->value);
-	XTS_ASSERT_EQUALS(NULL, c->next);
+	CHECK_EOF();
 
 	free_tokens(t);
 }
