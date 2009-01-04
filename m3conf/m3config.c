3f8ab6d8f1fc8cdddc70e413b637f863a3f0e2f5
Matt Lawrence <m3lawren@uwaterloo.ca>
2009-01-03 21:27:54 -0500

    Adding a very naive implementation of the lookup functions.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- m3conf/m3config.c
+++ m3conf/m3config.c
@@ -1,17 +1,58 @@
 #include <m3conf/m3config.h>
 
-struct m3config {
-	int dummy;
+#include <stdlib.h>
+#include <string.h>
+
+struct cnode {
+	struct cnode* next;
+
+	enum {
+		CN_SECTION,
+		CN_VALUE
+	}             type;
+
+	/* used for both CN_VALUE and CN_SECTION */
+	char*         key;
+	size_t        ksz;
+
+	/* only used for CN_VALUE */
+	char*         val;
+
+	/* only used for CN_SECTION */
+	struct cnode* child;
 };
 
+struct m3config {
+	struct cnode* data;
+};
+
+static const struct cnode* find_key(const struct cnode* start, const char* key) {
+	while (start) {
+		if (start->type == CN_SECTION && strncmp(start->key, key, start->ksz)) {
+			const struct cnode* r = find_key(start, key + (start->ksz + 1));
+			if (r) {
+				return r;
+			}
+		} else if (start->type == CN_VALUE && strcmp(start->key, key) == 0) {
+			return start;
+		}
+		start = start->next;
+	}
+	return NULL;
+}
+
 const char* m3conf_get_str(const struct m3config* c, const char* key, const char* def) {
-	(void)c;
-	(void)key;
-	return def;
+	const struct cnode* n = find_key(c->data, key);
+	if (!n) {
+		return def;
+	}
+	return n->val;
 }
 
 int m3conf_get_int(const struct m3config* c, const char* key, int def) {
-	(void)c;
-	(void)key;
-	return def;
+	const struct cnode* n = find_key(c->data, key);
+	if (!n) {
+		return def;
+	}
+	return atoi(n->val);
 }
